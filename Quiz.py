e754ad2253e9e7b0c942aff842d0f6bfd3acc5e6
mohammedekram051 <mohammedekram051@gmail.com>
2025-08-21 03:57:06 -0700

    Uploaded by EKRAM

---
+++ Quiz.py
@@ -0,0 +1,94 @@
+Q1="""one of the following is the si unit of electric current?
+
+A,coulomb B,newton C,ampere D,volt"""
+
+Q2="""which methods of charging a body leaves the charge body and the uncharged body with the opposite sign of charge?
+
+A,conduction B,induction C,rubbing D,none"""
+
+Q3="""a conducter possesses a negative charge of -0.5*10**-19c how many electrons does it have?
+
+A,4.053electrons B,0.312electrons C,0.405electrons D,3.12electrons"""
+
+Q4="""which of the following alternative have different unit?
+
+A,speed and velocity B,force and weight C,speed and distance D,distance and displacement"""
+
+Q5="""the rate of change of velocity in a given time interval
+
+A,displacement B,acceleration C,speed D,position"""
+
+Q6="""the concentration of 10g of NOH is 500ml of asolution is
+
+A,0.5m B,0.05m  C,5.0m C,2.0m"""
+
+Q7="""what is the malarity of solution obtained dissolving 0.01 mol Nacl in 0.5l solution
+
+A,0.5m B,0.02m C,20m D,50m"""
+
+Q8="""all of the following are amphotricoxide except
+
+A,Zno B,Sno C,Pbo D,Sro"""
+
+Q9="""oxides that donot react with acid or bases are knownas
+
+A,acidoxide B,neutraloxide C,acidicoxide D,basicoxide"""
+
+Q10="""which one of the following is the last dense?
+
+A,Au B,Hg C,Al D,Na"""
+
+Q11="""the reference angle of 130 degree is
+
+A,30degree  B,50degree  C,900degree  D,1080degree E,810degree"""
+
+Q12="""if a is an obtuse angle and sine A=4/5 then sec A?
+A,-3/5 B,-4/5 C,5/4 D,-5/3"""
+
+Q13="""Which of the following indicate the fourth qudrant angle X?
+
+A,sinx>0 and cosx<0 B,sinx<0and cosx<0 C,cosx>0and tanx<0 D,sinx>0and tanx<0"""
+
+Q14="""what is the mantissa and the characteristic of log 1/8
+A,1.25and1 B,1.25and -1  C,1and8 D, -1 and 1.25"""
+Q15="""THE relation R={(x,y): y=x**2} then which orderd pairs belongs to R ?
+A,(-2,-4) B,(1/4,1/8) C,(1,2) D,(3,6)"""
+
+Q16="""which of the following human blood type lacks anti body?
+A,O B,A C,B D,A and B"""
+
+Q17=""" in which structure of the lung does exchange of gases with the blood take place
+
+A,capilaries B,bronchioles C,alveoli D,bronchi"""
+
+Q18="""what are primary functions of the lymohatic system?
+
+A,to transport oxygen B,to produce digestive system C,to filter return excess tissue fluid to the blood D,to regulate body temperature"""
+
+Q19="""the function unit of the kidney is
+
+A,nephron B,glomerular C,tubular D,none"""
+Q20="""one is voice box it is where the sound is produce
+A,pharynx B,larynx C,bronchi D,trachia"""
+
+Q21="""the modal that we use it to give strong warning or advice
+
+A,had better B,suggest C,must D,should"""
+Q22="""we always   our  assignment in english 
+
+A,discussing B,discuss C,discussed D,was discussing"""
+Q23="""  is a word that takes the place of noun
+
+A,verbs B,word C,pronoun D,noun"""
+Q24="""the sequence of numbers that are based on addition and subtraction is
+A,geometric sequence B,arithmetic sequence C,geometric series D,pattern"""
+
+Q25="""which one of the following orderd pairs lie in second quadrant?
+
+A,(2,4)  B(1,-2)  C(-2,-3)  D,(-4,5)"""
+
+Questions={Q1:'c' ,Q2:'A',Q3:'B',Q4:'C',Q5:'B',Q6:'A',Q7:'B',Q8:'D',Q9:'B',Q10:'D',Q11:'B',Q12:'D',Q13:'C',Q14:'B',
+Q15:'C',Q16:'D',Q17:'C',Q18:'A',Q19:'A',Q20:'C',Q21:'C',Q22:'C',Q23:'B',Q24:'B',Q25:'D'}
+    
+
+
